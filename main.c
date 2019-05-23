7beaa79f958da3ef80adbb545c01e1c8b362ff46
Allan <allan.oliveira16@fatec.sp.gov.br>
2019-05-23 11:09:11 -0300

    primeira versão

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <stdbool.h>
+#define ROD 10
+#define CID 50
+
+typedef char nomeRodovia;
+typedef char nomeCidade;
+
+
+
+typedef struct aux{
+  nomeCidade A[CID];
+  struct aux* prox;
+}CIDADES;
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
