13193bfe0280a5bc3ec3423cbcd4b5949fa2ef43
Allan Silva Barroso de Oliveira <allan.oliveira16@fatec.sp.gov.br>
2019-05-27 20:10:44 -0300

    Tentativa de resolução do ep2

--- teste.c
+++ teste.c
@@ -2,8 +2,17 @@
 #include <stdlib.h>
 
 int main() {
+  FILE *arquivo = NULL;
+  char palavra[20];
 
-  printf("Hello world!\n");
+  printf("Informe o nome do arquivo: ");
+  scanf("%s", &palavra);
+  arquivo = fopen(palavra, "a");
 
+  if (arquivo == NULL){
+    printf("Erro na abertura do arquivo!\n");
+  }else{
+    printf("O arquivo foi aberto!\n");
+  }
   return 0;
 }
