5c9092a39fbcf4057e5aab57282ab42a0ac161ec
Allan <allan.oliveira16@fatec.sp.gov.br>
2019-05-29 16:37:44 -0300

    Resolução de lista ligada de Cidades

--- ep1.c
+++ ep1.c
@@ -14,72 +14,99 @@
 
 typedef char NOMECIDADE;
 
-typedef struct{
-  NOMECIDADE A[CID];
-}REGISTRO;
+typedef struct
+{
+    NOMECIDADE A[CID];
+} REGISTRO;
 
-typedef struct aux{
-  REGISTRO reg;
-  struct aux* ant;
-  struct aux* prox;
-}CIDADE;
+typedef struct aux
+{
+    REGISTRO reg;
+    struct aux* ant;
+    struct aux* prox;
+} CIDADE;
 
 typedef CIDADE* PONT;
 
-typedef struct{
-  PONT inicio;
-}LISTA;
+typedef struct
+{
+    PONT inicio;
+} LISTA;
 
-void inicializarLista (LISTA* l){
-  l->inicio = NULL;
+void inicializarLista (LISTA* l)
+{
+    l->inicio = NULL;
 }
 
-void finalizarLista (LISTA* l){
-  PONT end = l->inicio;
-  while (end != NULL) {
-    PONT apagar = end;
-    end = end->prox;
-    free(apagar);
-  }
-  l->inicio = NULL;
+void finalizarLista (LISTA* l)
+{
+    PONT end = l->inicio;
+    while (end != NULL)
+    {
+        PONT apagar = end;
+        end = end->prox;
+        free(apagar);
+    }
+    l->inicio = NULL;
 }
 
-void exibirLista(LISTA* l){
-  PONT end = l->inicio;
-  printf("--- inicio da lista---\n\n");
-  while(end != NULL) {
-    printf("- %s\n", end->reg.A);
-    end = end->prox;
-  }
-  printf("\n--- fim da lista ---\n");
+void exibirLista(LISTA* l)
+{
+    PONT end = l->inicio;
+    printf("--- inicio da lista---\n\n");
+    while(end != NULL)
+    {
+        printf("- %s\n", end->reg.A);
+        end = end->prox;
+    }
+    printf("\n--- fim da lista ---\n");
 }
 
-PONT buscaSequencialExc(LISTA* l, NOMECIDADE ch, PONT* ant){
-  *ant = NULL;
-  PONT atual = l->inicio;
-  while ((atual != NULL) && (atual->reg.A < ch)) {
-    *ant = atual;
-    atual = atual->prox;
-  }
-  if((atual != NULL) && (strcmp(atual->reg.A, ch) == 0)) return atual;
-  return NULL;
+int comparar(REGISTRO ch, PONT atual)
+{
+    return strcmp(atual->reg.A, ch.A);
 }
 
-bool inserirElemListaOrd(LISTA* l, REGISTRO reg){
-  NOMECIDADE ch = reg.A;
-  PONT ant, i;
-  i = buscaSequencialExc(l, ch, &ant);
-  if(i != NULL) return false;
-  i = (PONT) malloc(sizeof(CIDADE));
-  i->reg = reg;
-  if(ant == NULL){
-    i->prox = l->inicio;
-    l->inicio = i;
-  }else{
-    i->prox = ant->prox;
-    ant->prox = i;
-  }
-  return true;
+
+PONT buscaSequencialExc(LISTA* l, REGISTRO ch, PONT* ant)
+{
+    *ant = NULL;
+    PONT atual = l->inicio;
+    int i;
+    while (atual != NULL)
+    {
+        i = comparar(ch, atual->reg.A);
+        if( i < 0){
+            *ant = atual;
+            atual = atual->prox;
+        }else{
+            break;
+        }
+    }
+    if((atual != NULL) && (strcmp(ch.A, atual->reg.A) == 0))
+        return atual;
+    return NULL;
+}
+
+bool inserirElemListaOrd(LISTA* l, REGISTRO reg)
+{
+    PONT ant, i;
+    i = buscaSequencialExc(l, reg, &ant);
+    if(i != NULL)
+        return false;
+    i = (PONT) malloc(sizeof(CIDADE));
+    i->reg = reg;
+    if(ant == NULL)
+    {
+        i->prox = l->inicio;
+        l->inicio = i;
+    }
+    else
+    {
+        i->prox = ant->prox;
+        ant->prox = i;
+    }
+    return true;
 }
 
 void menu()
@@ -96,7 +123,7 @@ void menu()
 
 void limpaTela()
 {
-    system("cls");
+    system("clear");
 }
 
 void perguntaVolta()
@@ -105,80 +132,89 @@ void perguntaVolta()
     printf("[ 1 ] Voltar\n");
 }
 
-int main(int argc, char const *argv[]) {
-  LISTA* l = (LISTA*) malloc(sizeof(LISTA));
-  REGISTRO reg;
-  bool volta;
-  int opcao=1, pergunta;
-  PONT retorno;
+int main(int argc, char const *argv[])
+{
+    LISTA* l = (LISTA*) malloc(sizeof(LISTA));
+    REGISTRO reg;
+    bool volta;
+    int opcao=1, pergunta;
+    PONT retorno;
 
-  inicializarLista (l);
+    inicializarLista (l);
 
-  while(true)
-  {
-      limpaTela();
-      menu();
-      do{
-          scanf("%d",&opcao);
-      }while(opcao<1 || opcao>4);
+    while(true)
+    {
+        limpaTela();
+        menu();
+        do
+        {
+            scanf("%d",&opcao);
+        }
+        while(opcao<1 || opcao>4);
 
-      if(opcao==4)
-      {
-          return 0;
-      }
+        if(opcao==4)
+        {
+            return 0;
+        }
 
-      do{
-          switch(opcao)
-          {
-          case 1:
-          do{
-              limpaTela();
-              printf("registro: ");
-              scanf("%s",reg.A);
-              printf("\n");
-              inserirElemListaOrd(l, reg);
-              perguntaVolta();
-              printf("[ 2 ] Novo elemento\n");
-              do{
-                  scanf("%d",&pergunta);
-              }while(pergunta<1 || pergunta>2);
-          }while(pergunta==2);
-          break;
-          case 2:
-              limpaTela();
-              exibirLista(l);
-              perguntaVolta();
-              scanf("%d",&pergunta);
-              break;
-          case 3:
-              limpaTela();
-              /*
-              printf("elemento: ");
-              scanf("%d",&busca);
-              retorno = buscaSeqOrd(l, busca);
-              if(retorno==NULL)
-              {
-                  printf("Nao existe na lista\n\n");
-              }else{
-                  printf("Elemento [ %d ] da lista\n\n", retorno+1);
-              }*/
-              perguntaVolta();
-              scanf("%d",&pergunta);
-              break;
-          case 4:
-              limpaTela();
-              perguntaVolta();
-              scanf("%d",&pergunta);
-              break;
-          }
-          if(pergunta==0)
-          {
-              return 0;
-          }
-      }while(pergunta!=1);
-  }
+        do
+        {
+            switch(opcao)
+            {
+            case 1:
+                do
+                {
+                    limpaTela();
+                    printf("registro: ");
+                    scanf("%s",reg.A);
+                    printf("\n");
+                    inserirElemListaOrd(l, reg);
+                    perguntaVolta();
+                    printf("[ 2 ] Novo elemento\n");
+                    do
+                    {
+                        scanf("%d",&pergunta);
+                    }
+                    while(pergunta<1 || pergunta>2);
+                }
+                while(pergunta==2);
+                break;
+            case 2:
+                limpaTela();
+                exibirLista(l);
+                perguntaVolta();
+                scanf("%d",&pergunta);
+                break;
+            case 3:
+                limpaTela();
+                /*
+                printf("elemento: ");
+                scanf("%d",&busca);
+                retorno = buscaSeqOrd(l, busca);
+                if(retorno==NULL)
+                {
+                    printf("Nao existe na lista\n\n");
+                }else{
+                    printf("Elemento [ %d ] da lista\n\n", retorno+1);
+                }*/
+                perguntaVolta();
+                scanf("%d",&pergunta);
+                break;
+            case 4:
+                limpaTela();
+                perguntaVolta();
+                scanf("%d",&pergunta);
+                break;
+            }
+            if(pergunta==0)
+            {
+                return 0;
+            }
+        }
+        while(pergunta!=1);
+    }
 
-  finalizarLista(l);
-  free(l);
-  return 0;
+    finalizarLista(l);
+    free(l);
+    return 0;
 }
